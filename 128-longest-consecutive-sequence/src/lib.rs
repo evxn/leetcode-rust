1c02ae8d6d160f5faf4ea053fd42a06c16c2a670
Evgeniy Malyutin <evxn@users.noreply.github.com>
2024-07-20 13:30:58 +0400

    proper O(n) solution eliminating O(n²) loop NB: quadratic solution actually performs much faster on leetcode testsuite

--- 128-longest-consecutive-sequence/src/lib.rs
+++ 128-longest-consecutive-sequence/src/lib.rs
@@ -3,27 +3,50 @@
 pub struct Solution;
 
 impl Solution {
-    // time: O(n^2), max space: O(n)
+    // time: O(n), max space: O(n)
     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
         use std::collections::HashSet;
-        let uniq_nums: HashSet<_> = nums.into_iter().collect();
+        let mut uniq_nums: HashSet<_> = nums.into_iter().collect();
 
         let mut max_seq_len = 0;
 
-        // time O(n)
-        for &n in &uniq_nums {
-            if !uniq_nums.contains(&(n - 1)) {
-                // n is a start of a new seq
-                let mut seq_len = 1;
+        // proper O(n) loop
+        while let Some(&n) = uniq_nums.iter().next() {
+            uniq_nums.remove(&n);
 
-                // time O(n)
-                while uniq_nums.contains(&(n + seq_len)) {
-                    seq_len += 1;
-                }
+            // n is part of a new seq
+            let mut seq_len = 1;
 
-                max_seq_len = max_seq_len.max(seq_len);
+            let mut current = n;
+            // remove and count elements in the ascending order
+            while uniq_nums.remove(&(current + 1)) {
+                current += 1;
+                seq_len += 1;
             }
+
+            // reset current to n
+            current = n;
+            // remove and count elements in the decending order
+            while uniq_nums.remove(&(current - 1)) {
+                current -= 1;
+                seq_len += 1;
+            }
+
+            max_seq_len = max_seq_len.max(seq_len);
         }
+
+        // NB: simpler O(n²) loop performs much faster on leetcode testsuite
+        // for &n in &uniq_nums {
+        //      if !uniq_nums.contains(&(n - 1)) {
+        //          // n is a start of a new seq
+        //          let mut seq_len = 1;
+        //          while uniq_nums.contains(&(n + seq_len)) {
+        //              seq_len += 1;
+        //          }
+        //
+        //          max_seq_len = max_seq_len.max(seq_len);
+        //      }
+        //  }
 
         max_seq_len
     }
